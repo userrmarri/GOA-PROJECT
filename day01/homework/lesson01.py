c5ae06b937055e85ebf35bbdb63a81e70d461a9a
userrmarri <m.gogebashvili26@gmail.com>
2024-11-13 23:20:05 +0400

    commit

---
+++ day01/homework/lesson01.py
@@ -0,0 +1,12 @@
+name = "mari"
+print (name)
+surname = "gogebashvili"
+age = 13
+height = 1.55
+print (name + " " + surname)
+print (type(age))
+print (type(name))
+print (type(surname))
+print (type(height))
+print (name +  " " + str(age))
+print (name + " " + surname + " " + str(age) + " " +  str(height)) 
