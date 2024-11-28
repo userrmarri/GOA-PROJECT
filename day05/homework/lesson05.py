5a9c4f73c6ff493d83ba841016e71cb0cc48d984
userrmarri <m.gogebashvili26@gmail.com>
2024-11-29 00:03:00 +0400

    commit

---
+++ day05/homework/lesson05.py
@@ -0,0 +1,34 @@
+# unda gavasworot kodi
+
+
+# 1
+current_year = 2024
+print("The current year is " + current_year)
+
+#es kodi sworia
+
+
+# 2
+#country = "USA"
+# print("You are from " + Country)
+
+# 2
+country = "USA"
+print("You are from") + ("Country")
+
+
+# 3
+height = 180
+print("Your height in cm is " + height / 100 + " meters")
+
+#sworia es kodi
+
+
+# 4
+# print("Hello, " + new_name)
+# new_name = "Alex"
+
+
+# 4
+new_name = "Alex"
+print("Hello, " + new_name)
---
+++ day07/homework/lesson07.py
@@ -0,0 +1,8 @@
+name = ("mari")
+surname = ("gogebashvili")
+age = (13)
+print (name + " " + surname + " " + age)
+
+A = ("10+5")
+a = ("10-5")
+print (A + a)
