dc1ea62e520123ca1c64ed3b0eca7e528cc8b7c9
userrmarri <m.gogebashvili26@gmail.com>
2024-12-10 22:04:11 +0400

    commit

---
+++ day09/classwork/lesson09.py
@@ -0,0 +1,9 @@
+print(input = "what is your number")
+print(input = "what is your number")
+a = 10
+b = 5
+
+a - b
+a + b
+a * b
+a / b
---
+++ day09/homework/lesson09.py
@@ -0,0 +1,8 @@
+username = input ("enter your username")
+birth_date = input ("enter your birth date") 
+
+# intejeri aris  sityva mara annutan arsebobs intejeri ricxvi
+# int-(5) (intejeri)
+#str-("5") (stringi)
+
+#concatenation anu konkatenacia aris ori an meeti stringis sheerteba
