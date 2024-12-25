20b9d20de30e0e7c3a527a94143b43f66c74b20d
userrmarri <m.gogebashvili26@gmail.com>
2024-12-26 01:26:56 +0400

    commit

---
+++ day11/classwork/lesson11.py
@@ -0,0 +1,72 @@
+# 1
+
+# aq yvela true-s gamoitans
+print(10 > 5)
+print(7  > 5)
+print(20 > 10)
+print(10 > 8)
+print(34 > 30)
+# aq yvela false-s gamoitans
+print(1  > 3) 
+print(3 > 23)
+print(2 > 6)
+print(3 > 3)
+print(1 > 10)
+
+# #aq yvela true-s gamoitans 
+print(10 < 20)
+print(6 < 14)
+print(3 < 5)
+print(8 < 18)
+print(9 < 40)
+# #aq yvela false-s gamoitans
+print(34 < 3)
+print(58 < 9)
+print(446 < 200)
+print(757 < 202)
+print(97 < 21)
+
+#aq yvela true-s gamoitans
+print(7  != 22)
+print(20 != 10)
+print(4  != 2)
+print(0  != 1)
+print(44 != 50)
+#aq yvela false-s gamoitans
+print(2  != 2) 
+print(53 != 53)
+print(34 != 34)
+print(42 != 42) 
+print(43 != 43)
+
+# #aq yvela true-s gamoitans
+print(60 == 60)
+print(10 == 10)
+print(54 == 54)
+print(0 == 0)
+print(50 == 50)
+# #aq yvela false-s gamoitans
+print(66 == 271) 
+print(33 == 43)
+print(332 == 55)
+print(44 == 5) 
+print(3 == 5)
+
+
+# 2
+stringi1= "hello"
+stringi2= "hello"
+
+print(stringi1 == stringi2)
+print(stringi1 != stringi2) 
+print(stringi1 > stringi2)
+print(stringi1 < stringi2) 
+
+stringi1 = "cats"
+stringi2 = "penguin"
+
+print(stringi1 == stringi2)
+print(stringi1 != stringi2) 
+print(stringi1 > stringi2)
+print(stringi1 < stringi2)
+
