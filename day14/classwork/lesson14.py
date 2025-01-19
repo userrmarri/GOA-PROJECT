d81c71aca0b596a48f9b579661d6a596550963de
userrmarri <m.gogebashvili26@gmail.com>
2025-01-19 17:02:57 +0400

    commit

---
+++ day14/classwork/lesson14.py
@@ -0,0 +1,1 @@
+print = input("if you are 18 years old and if you have driving lisence you  can go in country")
---
+++ day14/homework/lesson14.py
@@ -0,0 +1,4 @@
+num = int(input("enter number: "))
+if 5 < num <15 and num !=10:
+    print ("num is between 5 and 15, but the number is not 10")
+    
