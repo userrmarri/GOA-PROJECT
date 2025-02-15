8042f43e923d4617344040fe83e07187688d5b5d
userrmarri <m.gogebashvili26@gmail.com>
2025-02-15 18:22:17 +0400

    commit

---
+++ day18/classwork/lesson18.py
@@ -0,0 +1,16 @@
+for  i in range (40):
+print (i)
+
+random =("1 2 3 4")
+for random in random
+print (random)
+
+
+
+print (input (please enter your name))
+for i in range (15):
+
+
+
+for i in range (2, 20)
+print (i)
---
+++ day18/homework/lesson18.py
@@ -0,0 +1,6 @@
+print (input ("goa  best"))
+for i in range (100):
+    print (i)
+
+
+
