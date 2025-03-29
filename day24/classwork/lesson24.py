133e0847dafd2d026cadcfe624fbba009990770e
userrmarri <m.gogebashvili26@gmail.com>
2025-03-29 23:47:27 +0400

    commit

---
+++ day24/classwork/lesson24.py
@@ -0,0 +1,12 @@
+for i in range(20):
+if i == 15:
+continue
+print(i)
+
+
+num = 0
+while True:
+num += 1
+print(num)
+if num == 14:
+break
---
+++ day24/homework/lesson24.py
@@ -0,0 +1,6 @@
+for i in range (100)
+if i == 15 27 67 32 99 93:
+continue
+print(i)
+
+
